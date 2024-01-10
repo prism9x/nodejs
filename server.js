cd33c69c25b2cb6f89128521a165804bc13ff5de
Đinh Mạnh Tưởng <145707598+prism9x@users.noreply.github.com>
2024-01-10 18:15:25 +0700

    using express

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const http = require('node:http');
-const hostname = '127.0.0.1';
+const express = require('express')
+const app = express()
+const port = 3030
 
-// const port = 3000;
-const port = 3030;
+app.get('/', (req, res) => {
+    res.send('Prism9x - Hello World!')
+})
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Prism9x - Hello World !\n');
-});
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
